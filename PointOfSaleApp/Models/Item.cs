e1584f8b36d86217b0a5244a71c330bf820f6b7f
김현호 <hyundho12@gmail.com>
2023-09-11 17:26:04 +0900

    feat: Model Item,Order 생성

---
+++ PointOfSaleApp/Models/Item.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointOfSaleApp.Models
+{
+    public class Item
+    {
+        public string title { get; set; }
+        public int quantity { get; set; }
+        public double price { get; set; }
+        public string image { get; set; }
+
+        public double SubTotal
+        {
+            get => price * quantity;
+        }
+    }
+
+    public enum ItemCategory
+    {
+        Noodles,
+        Rice,
+        Appetizers,
+        Desserts,
+        Beverages
+    }
+}
---
+++ PointOfSaleApp/Models/Order.cs
@@ -0,0 +1,35 @@
+﻿
+namespace PointOfSaleApp.Models
+{
+    public class Order
+    {
+        public int Table { get; set; }
+        public double Tip { get; set; }
+        public byte[] SignatureData { get; set; }
+
+        public List<Item> items { get; set; }
+
+        public string States { get; set; }
+        public OrderType OrderType = OrderType.DineIn;
+
+        public string Total
+        {
+            get
+            {
+                var tot = items.Sum(i => (i.price * i.quantity));
+                if (Tip != 0)
+                {
+                    tot = tot + (tot * Tip);
+                }
+                return tot.ToString();
+            }
+        }
+    }
+
+    public enum OrderType
+    {
+        DineIn,
+        CarryOut,
+        Delivery
+    }
+}
