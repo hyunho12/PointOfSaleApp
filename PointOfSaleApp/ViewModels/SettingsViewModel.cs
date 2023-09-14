7cfc20b0300ab6e2a08d1a0b7c836d670fb3e60a
김현호 <hyundho12@gmail.com>
2023-09-14 11:55:27 +0900

    feat: settingPage, DashboardPage구현

---
+++ PointOfSaleApp/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,29 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using PointOfSaleApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointOfSaleApp.ViewModels
+{
+    public partial class SettingsViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        ObservableCollection<Item> _products;
+
+        public SettingsViewModel()
+        {
+            Products = new ObservableCollection<Item>(AppData.Items);
+        }
+
+        [RelayCommand]
+        public async Task NotImplemented()
+        {
+            await App.Current.MainPage.DisplayAlert("Not Implemented", "Wouldn't it be nice?", "Okay");
+        }
+    }
+}
