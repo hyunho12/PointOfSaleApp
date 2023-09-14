1ac97bf06d917c2e0ef73ac7ca4f7edba01fe5c5
김현호 <hyundho12@gmail.com>
2023-09-14 17:30:46 +0900

    1.orderPage에서 orderDetailpage로 화면이동시 command binding이 적용이 안됨. 2. OrdersPage.xaml 비하인드코드참조, 비하인드코드에서 바인딩컨텍스트연결시 이에러가 왜뜨는지?

--- PointOfSaleApp/Pages/Mobile/OrdersPage.xaml.cs
+++ PointOfSaleApp/Pages/Mobile/OrdersPage.xaml.cs
@@ -1,9 +1,20 @@
+using Android.Icu.Number;
+using PointOfSaleApp.ViewModels;
+
 namespace PointOfSaleApp.Pages.Mobile;
 
 public partial class OrdersPage : ContentPage
-{
+{	
 	public OrdersPage()
 	{
-		InitializeComponent();
+		InitializeComponent();		
 	}
+
+//   public OrdersViewModel ViewModel;
+//   public OrdersPage(OrdersViewModel vm)
+//   {
+//       InitializeComponent();
+//		 BindingContext = vm;
+ //  }
+//: 'No parameterless constructor defined for type 'Point Of Sale App.Pages.Mobile.Orders Page
 }
--- PointOfSaleApp/ViewModels/OrdersViewModel.cs
+++ PointOfSaleApp/ViewModels/OrdersViewModel.cs
@@ -1,11 +1,9 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using PointOfSaleApp.Models;
-using System;
-using System.Collections.Generic;
+using PointOfSaleApp.Pages.Mobile;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace PointOfSaleApp.ViewModels
 {
@@ -14,9 +12,28 @@ namespace PointOfSaleApp.ViewModels
         [ObservableProperty]
         private ObservableCollection<Order> _orders;
 
+        [RelayCommand]
+        private async Task PayAsync(object obj)
+        {
+            try
+            {
+                var navigationParameter = new Dictionary<string, object>
+                {
+                    {"Order",this }
+                };
+                await Shell.Current.GoToAsync($"{nameof(OrderDetailsPage)}", navigationParameter);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            //await Shell.Current.GoToAsync("OrderDetailsPage");
+        }
+
         public OrdersViewModel() 
         {
             Orders = new ObservableCollection<Order>(AppData.Orders);
         }
+        
     }
 }
